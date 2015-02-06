0841f5e950ac2f43adad4e83dedb67b07d4e6b3c
Rachel Welch <rachelmsmeltzer@gmail.com>
2015-02-05 19:53:26 -0700

    complete project

--- app.js
+++ app.js
@@ -1,11 +1,62 @@
 $(document).ready(function() {
+    
+    // GET request to get user data
    $('#getUsers').on('click', function() {
        return $.ajax({
        method: 'GET',
        url: 'http://reqr.es/api/users?page=1',
-       success: function(data) {
-         console.log(data);  
+       success: function(res) {
+         insertData(res.data);  
        },
      })
    });
+    
+    // Create a new user using POST method
+    $('#addUser').on('click', function(e) {
+        e.preventDefault();
+       var userName = $('#name').val();
+       var userJob = $('#job').val();
+       return $.ajax({
+           url: 'http://reqr.es/api/users',
+           method: 'POST',
+           data: {
+             name: userName,
+             job: userJob
+           },
+           success: function(res) {
+            $('#recentUser').html(
+            '<li>' +
+                'name: ' + res.name +
+            '</li>' +
+            '<li>' +
+                'job: ' + res.job +
+            '</li>' +
+            '<li>' +
+                'id: ' + res.id +
+            '</li>' +
+            '<li>' +
+                'created at: ' + res.createdAt +
+            '</li>'
+        )
+      }
+    })
+  });
+    
+    // Add info from the GET request into the DOM
+    var insertData = function(arr) {
+    for (var i = 0; i < arr.length; i++) {
+      $('#userInfo' + (i + 1)).html('<div>' +
+        'User Info:' +
+        '<li>' +
+        'First name: ' + arr[i].first_name +
+        '</li>' +
+        '<li>' +
+        'Last name: ' + arr[i].last_name +
+        '</li>' +
+        '<hr>' +
+        '</div>'
+      )
+    };
+  };
+    
 });
